b31bd4180eaf0377d67a5ad50d9f75e6bd25ad30
Juián David Buitrago Orozco <jdbuitrago@peoplecontact.com.co>
2021-04-26 16:07:13 -0500

    sent data

--- app/src/main/java/com/example/formulario/MainActivity.java
+++ app/src/main/java/com/example/formulario/MainActivity.java
@@ -6,8 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,62 +34,56 @@ public class MainActivity extends AppCompatActivity {
     private static final String FILE_NAME = "texto.txt";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setUpView();
-    }
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-    private void setUpView() {
-        documentTypeStudent = findViewById(R.id.documentTypeStudent);
-        documentNumberStudent = findViewById(R.id.documentNumberStudent);
-        codeStudent = findViewById(R.id.codeStudent);
-        nameStudent = findViewById(R.id.nameStudent);
-        surnameStudent = findViewById(R.id.surnameStudent);
-        sexStudent = findViewById(R.id.sexStudent);
-        emailStudent = findViewById(R.id.emailStudent);
-        phoneStudent = findViewById(R.id.phoneStudent);
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveFile();
-            }
-        });
-    }
-    private void saveFile(){
-        String textoASalvar = documentTypeStudent.getText().toString();
-        String textoASalvar1 = documentNumberStudent.getText().toString();
-        String textoASalvar2 = codeStudent.getText().toString();
-        String textoASalvar3 = nameStudent.getText().toString();
-        String textoASalvar4 = surnameStudent.getText().toString();
-        String textoASalvar5 = sexStudent.getText().toString();
-        String textoASalvar6 = emailStudent.getText().toString();
-        String textoASalvar7 = phoneStudent.getText().toString();
-        FileOutputStream fileOutputStream = null;
+            documentTypeStudent = findViewById(R.id.documentTypeStudent);
+            documentNumberStudent = findViewById(R.id.documentNumberStudent);
+            codeStudent = findViewById(R.id.codeStudent);
+            nameStudent = findViewById(R.id.nameStudent);
+            surnameStudent = findViewById(R.id.surnameStudent);
+            sexStudent = findViewById(R.id.sexStudent);
+            emailStudent = findViewById(R.id.emailStudent);
+            phoneStudent = findViewById(R.id.phoneStudent);
+            btnSave = findViewById(R.id.btnSave);
 
-        try{
-            fileOutputStream.write(textoASalvar.getBytes());
-            fileOutputStream.write(textoASalvar1.getBytes());
-            fileOutputStream.write(textoASalvar2.getBytes());
-            fileOutputStream.write(textoASalvar3.getBytes());
-            fileOutputStream.write(textoASalvar4.getBytes());
-            fileOutputStream.write(textoASalvar5.getBytes());
-            fileOutputStream.write(textoASalvar6.getBytes());
-            fileOutputStream.write(textoASalvar7.getBytes());
-            Log.d(tag:"TAG1" msg: "fichero salvado en" + getFilesDir() + "/" + FILE_NAME);
-
-        }catch(Exception e){
-            e.printStackTrace();
-        }finaly {
-            if(fileOutputStream != null){
-                try{
-                    fileOutputStream.close();
-                }catch (Exception e){
-                    e.printStackTrace();
+            btnSave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ejecutarServicio("http://localhost:8080/developeru/insertar_estudiante.php");
                 }
-            }
-
+            });
         }
 
+        private void ejecutarServicio(String URL){
+            StringRequest stringRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String Response){
+                    Toast.makeText(getApplicationContext(), "OPERACION EXITOSA", Toast.LENGTH_SHORT).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error){
+                    Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                }
+
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> parametros=new HashMap<String, String>();
+                    parametros.put("documentNumber",documentNumberStudent.getText().toString());
+                    parametros.put("documentType",documentTypeStudent.getText().toString());
+                    parametros.put("code",codeStudent.getText().toString());
+                    parametros.put("sex",sexStudent.getText().toString());
+                    parametros.put("surname",surnameStudent.getText().toString());
+                    parametros.put("email",emailStudent.getText().toString());
+                    parametros.put("name",nameStudent.getText().toString());
+                    parametros.put("phone",phoneStudent.getText().toString());
+                    return parametros;
+                }
+            };
+            RequestQueue requestQueue= Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+        }
     }
-}
